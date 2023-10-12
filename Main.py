1fd815580e44ce95a8b7fa3931a2ef5bc93eabbe
HY_ElCositas <61937572+VanimacionYT@users.noreply.github.com>
2023-10-12 12:20:40 +0200

    Created Exit Key Combo (not working, WIP)

--- Main.py
+++ Main.py
@@ -7,21 +7,38 @@ KeyCombo = [
     {keyboard.Key.shift, keyboard.KeyCode(char = "A")},
     {keyboard.Key.alt_l, keyboard.KeyCode(char = "1")},
     {keyboard.KeyCode(char = "b")},
-    {keyboard.KeyCode(char = "B")}
+    {keyboard.KeyCode(char = "B")},
 ]
+
+ExitKeyCombo = [
+    {keyboard.Key.esc}
+]
+
 current = set()
 
 def execute():
     print("Tecla Pulsada")
+    print(current)
+
+def terminate():
+    print("Terminé")
+    print(current)
+    exit()
 
 def on_press(key):
     if any({key in Combo for Combo in KeyCombo}):
         current.add(key)
         if any(all(k in current for k in Combo)for Combo in KeyCombo):
             execute()
+    if any({key in Combo for Combo in ExitKeyCombo}):
+        current.add(key)
+        if any(all(k in current for k in Combo)for Combo in ExitKeyCombo):
+            terminate()
 
 def on_release(key):
     if any({key in Combo for Combo in KeyCombo}):
+        current.remove(key)
+    if any({key in Combo for Combo in ExitKeyCombo}):
         current.remove(key)
 
 while True:
